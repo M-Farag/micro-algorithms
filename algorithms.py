578eb7069cf5fa4835368faa426734fdd011a2e7
Mina Farag <42836754+M-Farag@users.noreply.github.com>
2021-03-31 23:40:01 +0200

    Feature/20210331 add calc remaining units for a goal (#3)
    * ADD: Algorithm to calculate and return the remaining units for a goal
    
    * OPT: Algorithm input types

--- algorithms.py
+++ algorithms.py
@@ -5,5 +5,5 @@ def discount_and_return_new_value(amount: float,discount: float):
     return amount * ( 1 - (discount/100))
 
 # Calculate and return the remaining units for a goal
-def remaining_units_towards_a_goal(aimed_amount,accomplished_amount,progress_rate_per_unit):
+def remaining_units_towards_a_goal(aimed_amount: float,accomplished_amount: float,progress_rate_per_unit: float):
     return (aimed_amount - accomplished_amount) / progress_rate_per_unit

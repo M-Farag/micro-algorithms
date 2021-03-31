00c3d24fcdaa24f7cfd9302dd21a69ef1332f8b8
M-Farag <mina.farag@icloud.com>
2021-03-31 23:32:02 +0200

    ADD: Algorithm to calculate and return the remaining units for a goal

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,9 @@
-# Return a new amount after applying a discount on the original amount
-# Check the wiki for more information about this algorithm
+# Check the wiki for more information and the use cases of any algorithm
+
+# Calculate and return a new amount after applying a discount on the original amount
 def discount_and_return_new_value(amount: float,discount: float):
     return amount * ( 1 - (discount/100))
+
+# Calculate and return the remaining units for a goal
+def remaining_units_towards_a_goal(aimed_amount,accomplished_amount,progress_rate_per_unit):
+    return (aimed_amount - accomplished_amount) / progress_rate_per_unit
